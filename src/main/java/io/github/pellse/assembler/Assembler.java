4199ce6f7e8f1ae4f84867dcac70591033144be3
pellse <sebastien_pelletier_787@hotmail.com>
2018-03-08 22:40:55 -0500

    Updating Assembler interface to match added method in implementations

--- src/main/java/io/github/pellse/assembler/Assembler.java
+++ src/main/java/io/github/pellse/assembler/Assembler.java
@@ -72,4 +72,54 @@ public interface Assembler<T, ID, IDC extends Collection<ID>, RC> {
             Mapper<ID, E6, IDC, Throwable> mapper6,
             Mapper<ID, E7, IDC, Throwable> mapper7,
             Function8<T, E1, E2, E3, E4, E5, E6, E7, R> domainObjectBuilderFunction);
+
+    <E1, E2, E3, E4, E5, E6, E7, E8, R> RC assemble(
+            Mapper<ID, E1, IDC, Throwable> mapper1,
+            Mapper<ID, E2, IDC, Throwable> mapper2,
+            Mapper<ID, E3, IDC, Throwable> mapper3,
+            Mapper<ID, E4, IDC, Throwable> mapper4,
+            Mapper<ID, E5, IDC, Throwable> mapper5,
+            Mapper<ID, E6, IDC, Throwable> mapper6,
+            Mapper<ID, E7, IDC, Throwable> mapper7,
+            Mapper<ID, E8, IDC, Throwable> mapper8,
+            Function9<T, E1, E2, E3, E4, E5, E6, E7, E8, R> domainObjectBuilderFunction);
+
+    <E1, E2, E3, E4, E5, E6, E7, E8, E9, R> RC assemble(
+            Mapper<ID, E1, IDC, Throwable> mapper1,
+            Mapper<ID, E2, IDC, Throwable> mapper2,
+            Mapper<ID, E3, IDC, Throwable> mapper3,
+            Mapper<ID, E4, IDC, Throwable> mapper4,
+            Mapper<ID, E5, IDC, Throwable> mapper5,
+            Mapper<ID, E6, IDC, Throwable> mapper6,
+            Mapper<ID, E7, IDC, Throwable> mapper7,
+            Mapper<ID, E8, IDC, Throwable> mapper8,
+            Mapper<ID, E9, IDC, Throwable> mapper9,
+            Function10<T, E1, E2, E3, E4, E5, E6, E7, E8, E9, R> domainObjectBuilderFunction);
+
+    <E1, E2, E3, E4, E5, E6, E7, E8, E9, E10, R> RC assemble(
+            Mapper<ID, E1, IDC, Throwable> mapper1,
+            Mapper<ID, E2, IDC, Throwable> mapper2,
+            Mapper<ID, E3, IDC, Throwable> mapper3,
+            Mapper<ID, E4, IDC, Throwable> mapper4,
+            Mapper<ID, E5, IDC, Throwable> mapper5,
+            Mapper<ID, E6, IDC, Throwable> mapper6,
+            Mapper<ID, E7, IDC, Throwable> mapper7,
+            Mapper<ID, E8, IDC, Throwable> mapper8,
+            Mapper<ID, E9, IDC, Throwable> mapper9,
+            Mapper<ID, E10, IDC, Throwable> mapper10,
+            Function11<T, E1, E2, E3, E4, E5, E6, E7, E8, E9, E10, R> domainObjectBuilderFunction);
+
+    <E1, E2, E3, E4, E5, E6, E7, E8, E9, E10, E11, R> RC assemble(
+            Mapper<ID, E1, IDC, Throwable> mapper1,
+            Mapper<ID, E2, IDC, Throwable> mapper2,
+            Mapper<ID, E3, IDC, Throwable> mapper3,
+            Mapper<ID, E4, IDC, Throwable> mapper4,
+            Mapper<ID, E5, IDC, Throwable> mapper5,
+            Mapper<ID, E6, IDC, Throwable> mapper6,
+            Mapper<ID, E7, IDC, Throwable> mapper7,
+            Mapper<ID, E8, IDC, Throwable> mapper8,
+            Mapper<ID, E9, IDC, Throwable> mapper9,
+            Mapper<ID, E10, IDC, Throwable> mapper10,
+            Mapper<ID, E11, IDC, Throwable> mapper11,
+            Function12<T, E1, E2, E3, E4, E5, E6, E7, E8, E9, E10, E11, R> domainObjectBuilderFunction);
 }
