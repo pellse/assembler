5d0cf44d36b66135cccfdc8f4bbdb10b99e4f28c
pellse <sebastien_pelletier_787@hotmail.com>
2018-06-03 05:54:48 -0400

    Added support for Akka Stream Source (issue #3)

---
+++ assembler-akka-stream/src/main/java/io/github/pellse/assembler/akkastream/AkkaSourceAdapter.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2018 Sebastien Pelletier
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.github.pellse.assembler.akkastream;
+
+import akka.NotUsed;
+import akka.stream.javadsl.Source;
+import io.github.pellse.assembler.AssemblerAdapter;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.function.UnaryOperator;
+import java.util.stream.Stream;
+
+import static akka.stream.javadsl.Source.*;
+import static java.util.stream.Collectors.toList;
+
+public class AkkaSourceAdapter<ID, R> implements AssemblerAdapter<ID, R, Source<R, NotUsed>> {
+
+    private final UnaryOperator<Source<Map<ID, ?>, ?>> sourceTransformer;
+
+    private AkkaSourceAdapter(UnaryOperator<Source<Map<ID, ?>, ?>> sourceTransformer) {
+        this.sourceTransformer = sourceTransformer;
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public Source<R, NotUsed> convertMapperSources(Stream<Supplier<Map<ID, ?>>> sources,
+                                                   Function<List<Map<ID, ?>>, Stream<R>> domainObjectStreamBuilder) {
+
+        List<Source<Map<ID, ?>, ?>> akkaSources = sources
+                .map(this::createSource)
+                .collect(toList());
+
+        return zipN(akkaSources)
+                .flatMapConcat(mapperResults -> from(domainObjectStreamBuilder.apply(mapperResults)::iterator));
+    }
+
+    private Source<Map<ID, ?>, ?> createSource(Supplier<Map<ID, ?>> mappingSupplier) {
+        return sourceTransformer.apply(lazily(() -> single(mappingSupplier.get())));
+    }
+
+    public static <ID, R> AkkaSourceAdapter<ID, R> akkaSourceAdapter() {
+        return akkaSourceAdapter(false);
+    }
+
+    public static <ID, R> AkkaSourceAdapter<ID, R> akkaSourceAdapter(boolean async) {
+        return akkaSourceAdapter(source -> async ? source.async() : source);
+    }
+
+    public static <ID, R> AkkaSourceAdapter<ID, R> akkaSourceAdapter(UnaryOperator<Source<Map<ID, ?>, ?>> sourceTransformer) {
+        return new AkkaSourceAdapter<>(sourceTransformer);
+    }
+}
---
+++ assembler-akka-stream/src/test/java/io/github/pellse/assembler/akkastream/AkkaSourceAssemblerTest.java
@@ -0,0 +1,114 @@
+/*
+ * Copyright 2018 Sebastien Pelletier
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.github.pellse.assembler.akkastream;
+
+import akka.Done;
+import akka.NotUsed;
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.stream.ActorMaterializer;
+import akka.stream.Materializer;
+import akka.stream.javadsl.Sink;
+import akka.stream.javadsl.Source;
+import akka.testkit.javadsl.TestKit;
+import io.github.pellse.assembler.AssemblerTestUtils;
+import io.github.pellse.assembler.AssemblerTestUtils.*;
+import io.github.pellse.util.function.checked.UncheckedException;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.concurrent.CompletionStage;
+import java.util.concurrent.ExecutionException;
+
+import static io.github.pellse.assembler.Assembler.assemblerOf;
+import static io.github.pellse.assembler.AssemblerTestUtils.*;
+import static io.github.pellse.assembler.akkastream.AkkaSourceAdapter.akkaSourceAdapter;
+import static io.github.pellse.util.query.MapperUtils.oneToManyAsList;
+import static io.github.pellse.util.query.MapperUtils.oneToOne;
+import static java.util.Arrays.asList;
+
+public class AkkaSourceAssemblerTest {
+
+    private final ActorSystem system = ActorSystem.create();
+    private final Materializer mat = ActorMaterializer.create(system);
+
+    private List<Customer> getCustomers() {
+        return asList(customer1, customer2, customer3, customer1, customer2);
+    }
+
+    @Test
+    public void testAssemblerBuilderWithAkkaSource() throws Exception {
+
+        TestKit probe = new TestKit(system);
+
+        Source<Transaction, NotUsed> transactionSource = assemblerOf(Transaction.class)
+                .fromSupplier(this::getCustomers, Customer::getCustomerId)
+                .assembleWith(
+                        oneToOne(AssemblerTestUtils::getBillingInfoForCustomers, BillingInfo::getCustomerId, BillingInfo::new),
+                        oneToManyAsList(AssemblerTestUtils::getAllOrdersForCustomers, OrderItem::getCustomerId),
+                        Transaction::new)
+                .using(akkaSourceAdapter());
+
+        final CompletionStage<Done> future = transactionSource.runWith(
+                Sink.foreach(elem -> probe.getRef().tell(elem, ActorRef.noSender())), mat);
+
+        probe.expectMsgAllOf(transaction1, transaction2, transaction3, transaction1, transaction2);
+
+        future.toCompletableFuture().get();
+    }
+
+    @Test(expected = UncheckedException.class)
+    public void testAssemblerBuilderWithAkkaSourceWithError() throws Throwable {
+
+        Source<Transaction, NotUsed> transactionSource = assemblerOf(Transaction.class)
+                .fromSupplier(this::getCustomers, Customer::getCustomerId)
+                .assembleWith(
+                        oneToOne(AssemblerTestUtils::throwSQLException, BillingInfo::getCustomerId, BillingInfo::new),
+                        oneToManyAsList(AssemblerTestUtils::throwSQLException, OrderItem::getCustomerId),
+                        Transaction::new)
+                .using(akkaSourceAdapter(true));
+
+        final CompletionStage<Done> future = transactionSource.runWith(Sink.ignore(), mat);
+
+        try {
+            future.toCompletableFuture().get();
+        } catch( ExecutionException e) {
+            throw e.getCause();
+        }
+    }
+
+    @Test
+    public void testAssemblerBuilderWithAkkaSourceAsync() throws Exception {
+
+        TestKit probe = new TestKit(system);
+
+        Source<Transaction, NotUsed> transactionSource = assemblerOf(Transaction.class)
+                .fromSupplier(this::getCustomers, Customer::getCustomerId)
+                .assembleWith(
+                        oneToOne(AssemblerTestUtils::getBillingInfoForCustomers, BillingInfo::getCustomerId, BillingInfo::new),
+                        oneToManyAsList(AssemblerTestUtils::getAllOrdersForCustomers, OrderItem::getCustomerId),
+                        Transaction::new)
+                .using(akkaSourceAdapter(true));
+
+        final CompletionStage<Done> future = transactionSource.runWith(
+                Sink.foreach(elem -> probe.getRef().tell(elem, ActorRef.noSender())), mat);
+
+        probe.expectMsgAllOf(transaction1, transaction2, transaction3, transaction1, transaction2);
+
+        future.toCompletableFuture().get();
+    }
+}
