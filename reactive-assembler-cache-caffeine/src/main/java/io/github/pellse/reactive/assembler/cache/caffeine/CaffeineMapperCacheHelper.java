9805479ae562532615bf05bec931eb4db7d7cc19
pellse <spelletier@javatechnicalwealth.com>
2022-04-07 11:57:06 -0400

    Update Caffeine integration module to support caching of individual entities from assembler rule Publisher

--- reactive-assembler-cache-caffeine/src/main/java/io/github/pellse/reactive/assembler/cache/caffeine/CaffeineMapperCacheHelper.java
+++ reactive-assembler-cache-caffeine/src/main/java/io/github/pellse/reactive/assembler/cache/caffeine/CaffeineMapperCacheHelper.java
@@ -2,10 +2,8 @@ package io.github.pellse.reactive.assembler.cache.caffeine;
 
 import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
-import io.github.pellse.reactive.assembler.QueryCache;
-import org.reactivestreams.Publisher;
-import reactor.core.publisher.Mono;
 
+import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
 
@@ -13,20 +11,37 @@ import static com.github.benmanes.caffeine.cache.Caffeine.newBuilder;
 
 public interface CaffeineMapperCacheHelper {
 
-    static <ID, R> QueryCache<ID, R> cache() {
+    static <ID, R> io.github.pellse.reactive.assembler.Cache<ID, R> cache() {
         return cache(newBuilder());
     }
 
-    static <ID, R> QueryCache<ID, R> cache(Caffeine<Object, Object> caffeine) {
+    static <ID, R> io.github.pellse.reactive.assembler.Cache<ID, R> cache(Caffeine<Object, Object> caffeine) {
         return cache(caffeine, Caffeine::build);
     }
 
-    static <ID, R> QueryCache<ID, R> cache(Function<Caffeine<Object, Object>, Cache<Iterable<ID>, Mono<Map<ID, R>>>> builder) {
+    static <ID, R> io.github.pellse.reactive.assembler.Cache<ID, R> cache(Function<Caffeine<Object, Object>, Cache<ID, List<R>>> builder) {
         return cache(newBuilder(), builder);
     }
 
-    static <ID, R> QueryCache<ID, R> cache(Caffeine<Object, Object> caffeine,
-                                           Function<Caffeine<Object, Object>, Cache<Iterable<ID>, Mono<Map<ID, R>>>> builder) {
-        return builder.apply(caffeine)::get;
+    static <ID, R> io.github.pellse.reactive.assembler.Cache<ID, R> cache(
+            Caffeine<Object, Object> caffeine,
+            Function<Caffeine<Object, Object>, Cache<ID, List<R>>> builder
+    ) {
+        return cache(builder.apply(caffeine));
+    }
+
+    static <ID, R> io.github.pellse.reactive.assembler.Cache<ID, R> cache(Cache<ID, List<R>> delegateCache) {
+        return new io.github.pellse.reactive.assembler.Cache<>() {
+
+            @Override
+            public Map<ID, List<R>> getAllPresent(Iterable<ID> ids) {
+                return delegateCache.getAllPresent(ids);
+            }
+
+            @Override
+            public void putAll(Map<ID, List<R>> map) {
+                delegateCache.putAll(map);
+            }
+        };
     }
 }
--- reactive-assembler-cache-caffeine/src/test/java/io/github/pellse/reactive/assembler/cache/caffeine/TestAssemblerCaffeineCache.java
+++ reactive-assembler-cache-caffeine/src/test/java/io/github/pellse/reactive/assembler/cache/caffeine/TestAssemblerCaffeineCache.java
@@ -9,21 +9,20 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.reactivestreams.Publisher;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 import java.util.List;
-import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import static com.github.benmanes.caffeine.cache.Caffeine.newBuilder;
 import static io.github.pellse.assembler.AssemblerTestUtils.*;
 import static io.github.pellse.reactive.assembler.AssemblerBuilder.assemblerOf;
+import static io.github.pellse.reactive.assembler.Cache.cached;
 import static io.github.pellse.reactive.assembler.Mapper.rule;
-import static io.github.pellse.reactive.assembler.QueryCache.cached;
 import static io.github.pellse.reactive.assembler.RuleMapper.oneToMany;
 import static io.github.pellse.reactive.assembler.RuleMapper.oneToOne;
 import static io.github.pellse.reactive.assembler.cache.caffeine.CaffeineMapperCacheHelper.cache;
+import static java.time.Duration.ofSeconds;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestAssemblerCaffeineCache {
@@ -44,7 +43,7 @@ public class TestAssemblerCaffeineCache {
     }
 
     private Flux<Customer> getCustomers() {
-        return Flux.just(customer1, customer2, customer3, customer1, customer2, customer3);
+        return Flux.just(customer1, customer2, customer3, customer1, customer2, customer3, customer1, customer2, customer3);
     }
 
     @BeforeEach
@@ -56,25 +55,51 @@ public class TestAssemblerCaffeineCache {
     @Test
     public void testReusableAssemblerBuilderWithCaffeineCache() {
 
-        final Cache<Iterable<Long>, Mono<Map<Long, BillingInfo>>> billingInfoCache = newBuilder().maximumSize(10).build();
+        final Cache<Long, List<BillingInfo>> c1 = newBuilder().maximumSize(10).build();
+        final Cache<Long, List<OrderItem>> c2 = newBuilder().maximumSize(10).build();
 
         var assembler = assemblerOf(Transaction.class)
                 .withIdExtractor(Customer::customerId)
                 .withAssemblerRules(
-                        cached(rule(BillingInfo::customerId, oneToOne(this::getBillingInfos, BillingInfo::new)), billingInfoCache::get),
-                        cached(rule(OrderItem::customerId, oneToMany(this::getAllOrders)), cache(newBuilder().maximumSize(10))),
+                        rule(BillingInfo::customerId, oneToOne(cached(this::getBillingInfos, c1::getAllPresent, c1::putAll), BillingInfo::new)),
+                        rule(OrderItem::customerId, oneToMany(cached(this::getAllOrders, cache(c2)))),
                         Transaction::new)
                 .build();
 
         StepVerifier.create(getCustomers()
                         .window(3)
+                        .delayElements(ofSeconds(1))
                         .flatMapSequential(assembler::assemble))
                 .expectSubscription()
-                .expectNext(transaction1, transaction2, transaction3, transaction1, transaction2, transaction3)
+                .expectNext(transaction1, transaction2, transaction3, transaction1, transaction2, transaction3, transaction1, transaction2, transaction3)
                 .expectComplete()
                 .verify();
 
         assertEquals(1, billingInvocationCount.get());
         assertEquals(1, ordersInvocationCount.get());
+    }
+
+    @Test
+    public void testReusableAssemblerBuilderWithCaffeineCache2() {
+
+        var assembler = assemblerOf(Transaction.class)
+                .withIdExtractor(Customer::customerId)
+                .withAssemblerRules(
+                        rule(BillingInfo::customerId, oneToOne(cached(this::getBillingInfos, cache()), BillingInfo::new)),
+                        rule(OrderItem::customerId, oneToMany(cached(this::getAllOrders, cache(builder -> builder.maximumSize(10).build())))),
+                        Transaction::new)
+                .build();
+
+        StepVerifier.create(getCustomers()
+                        .window(2)
+                        .delayElements(ofSeconds(1))
+                        .flatMapSequential(assembler::assemble))
+                .expectSubscription()
+                .expectNext(transaction1, transaction2, transaction3, transaction1, transaction2, transaction3, transaction1, transaction2, transaction3)
+                .expectComplete()
+                .verify();
+
+        assertEquals(2, billingInvocationCount.get());
+        assertEquals(2, ordersInvocationCount.get());
     }
 }
