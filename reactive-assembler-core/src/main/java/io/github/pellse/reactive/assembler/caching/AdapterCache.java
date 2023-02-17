a64bff434d2e3be56d7f84e392db61db04d563b3
pellse <sebastien.pelletier@sekmed.ca>
2023-02-16 22:10:21 -0500

    Adding updateAll delegate function to adapterCache() and updateAllCallback to ObservableCacheFactory

--- reactive-assembler-core/src/main/java/io/github/pellse/reactive/assembler/caching/AdapterCache.java
+++ reactive-assembler-core/src/main/java/io/github/pellse/reactive/assembler/caching/AdapterCache.java
@@ -12,9 +12,17 @@ public interface AdapterCache {
     static <ID, R> Cache<ID, R> adapterCache(
             BiFunction<Iterable<ID>, Boolean, Mono<Map<ID, List<R>>>> getAll,
             Function<Map<ID, List<R>>, Mono<?>> putAll,
-            Function<Map<ID, List<R>>, Mono<?>> removeAll
-    ) {
+            Function<Map<ID, List<R>>, Mono<?>> removeAll) {
+        return adapterCache(getAll, putAll, removeAll, null);
+    }
+
+    static <ID, R> Cache<ID, R> adapterCache(
+            BiFunction<Iterable<ID>, Boolean, Mono<Map<ID, List<R>>>> getAll,
+            Function<Map<ID, List<R>>, Mono<?>> putAll,
+            Function<Map<ID, List<R>>, Mono<?>> removeAll,
+            BiFunction<Map<ID, List<R>>, Map<ID, List<R>>, Mono<?>> updateAll) {
         return new Cache<>() {
+
             @Override
             public Mono<Map<ID, List<R>>> getAll(Iterable<ID> ids, boolean computeIfAbsent) {
                 return getAll.apply(ids, computeIfAbsent);
@@ -28,6 +36,12 @@ public interface AdapterCache {
             @Override
             public Mono<?> removeAll(Map<ID, List<R>> map) {
                 return removeAll.apply(map);
+            }
+
+            @Override
+            public Mono<?> updateAll(Map<ID, List<R>> mapToAdd, Map<ID, List<R>> mapToRemove) {
+                BiFunction<Map<ID, List<R>>, Map<ID, List<R>>, Mono<?>> f = updateAll != null ? updateAll : Cache.super::updateAll;
+                return f.apply(mapToAdd, mapToRemove);
             }
         };
     }
--- reactive-assembler-core/src/main/java/io/github/pellse/reactive/assembler/caching/ObservableCacheFactory.java
+++ reactive-assembler-core/src/main/java/io/github/pellse/reactive/assembler/caching/ObservableCacheFactory.java
@@ -1,55 +1,40 @@
 package io.github.pellse.reactive.assembler.caching;
 
-import java.lang.System.Logger;
-import java.lang.System.Logger.Level;
 import java.util.List;
 import java.util.Map;
+import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 import java.util.function.Function;
 
 import static io.github.pellse.reactive.assembler.caching.AdapterCache.adapterCache;
 import static io.github.pellse.util.ObjectUtils.then;
-import static java.lang.System.getLogger;
 
 public interface ObservableCacheFactory {
-
-    static <ID, R, RRC> Function<CacheFactory<ID, R, RRC>, CacheFactory<ID, R, RRC>> loggableCache(Level level) {
-        return loggableCache(getLogger(ObservableCacheFactory.class.getName()), level);
-    }
-
-    static <ID, R, RRC> Function<CacheFactory<ID, R, RRC>, CacheFactory<ID, R, RRC>> loggableCache(String loggerName, Level level) {
-        return loggableCache(getLogger(loggerName), level);
-    }
-
-    static <ID, R, RRC> Function<CacheFactory<ID, R, RRC>, CacheFactory<ID, R, RRC>> loggableCache(Logger logger, Level level) {
-
-        Function<String, Consumer<Map<ID, List<R>>>> log = prefix -> map -> {
-            if (logger.isLoggable(level)) {
-                logger.log(level, "{0}: {1}", prefix, map);
-            }
-        };
-
-        return observableCache(log.apply("getAll"), log.apply("putAll"), log.apply("removeAll"));
-    }
 
     static <ID, R, RRC> Function<CacheFactory<ID, R, RRC>, CacheFactory<ID, R, RRC>> observableCache(
             Consumer<Map<ID, List<R>>> getAllCallback,
             Consumer<Map<ID, List<R>>> putAllCallback,
-            Consumer<Map<ID, List<R>>> removeAllCallback) {
+            Consumer<Map<ID, List<R>>> removeAllCallback,
+            BiConsumer<Map<ID, List<R>>, Map<ID, List<R>>> updateAllCallback) {
 
-        return cacheFactory -> observableCache(cacheFactory, getAllCallback, putAllCallback, removeAllCallback);
+        return cacheFactory -> observableCache(cacheFactory, getAllCallback, putAllCallback, removeAllCallback, updateAllCallback);
     }
 
     static <ID, R, RRC> CacheFactory<ID, R, RRC> observableCache(
             CacheFactory<ID, R, RRC> delegateCacheFactory,
             Consumer<Map<ID, List<R>>> getAllCallback,
             Consumer<Map<ID, List<R>>> putAllCallback,
-            Consumer<Map<ID, List<R>>> removeAllCallback) {
+            Consumer<Map<ID, List<R>>> removeAllCallback,
+            BiConsumer<Map<ID, List<R>>, Map<ID, List<R>>> updateAllCallback) {
 
         return (fetchFunction, context) -> then(delegateCacheFactory.create(fetchFunction, context), cache -> adapterCache(
-                (ids, computeIfAbsent) -> cache.getAll(ids, computeIfAbsent).doOnNext(getAllCallback),
-                map -> cache.putAll(map).doOnSuccess(__ -> putAllCallback.accept(map)),
-                map -> cache.removeAll(map).doOnSuccess(__ -> removeAllCallback.accept(map))));
-
+                        (ids, computeIfAbsent) -> then(cache.getAll(ids, computeIfAbsent),
+                                mono -> getAllCallback != null ? mono.doOnNext(getAllCallback) : mono),
+                        map -> then(cache.putAll(map),
+                                mono -> putAllCallback != null ?  mono.doOnNext(__ -> putAllCallback.accept(map)): mono),
+                        map -> then(cache.removeAll(map),
+                                mono -> removeAllCallback != null ? mono.doOnNext(__ -> removeAllCallback.accept(map)) : mono),
+                        (mapToAdd, mapToRemove) -> then(cache.updateAll(mapToAdd, mapToRemove),
+                                mono -> updateAllCallback != null ? mono.doOnNext(__ -> updateAllCallback.accept(mapToAdd, mapToRemove)) : mono)));
     }
 }
