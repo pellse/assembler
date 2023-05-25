e4ebdca584c2e49cc465510e7a4a18f3fc4b7334
pellse <sebastien.pelletier@sekmed.ca>
2023-05-25 16:05:14 -0400

    Adding `BatchRuleBuilder` and `BatchRule` for easier integration with Spring GraphQL

--- reactive-assembler-core/src/main/java/io/github/pellse/reactive/assembler/Rule.java
+++ reactive-assembler-core/src/main/java/io/github/pellse/reactive/assembler/Rule.java
@@ -16,16 +16,23 @@
 
 package io.github.pellse.reactive.assembler;
 
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.Collection;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
 import static io.github.pellse.reactive.assembler.RuleContext.ruleContext;
 import static io.github.pellse.util.ObjectUtils.then;
+import static io.github.pellse.util.collection.CollectionUtil.toStream;
+import static java.util.function.Function.identity;
+import static java.util.stream.Collectors.toMap;
+import static reactor.core.publisher.Flux.fromIterable;
 
 @FunctionalInterface
 public interface Rule<ID, RRC> extends Function<Iterable<ID>, Mono<Map<ID, RRC>>> {
@@ -55,5 +62,67 @@ public interface Rule<ID, RRC> extends Function<Iterable<ID>, Mono<Map<ID, RRC>>
             RuleContext<ID, IDC, R, RRC> ruleContext,
             RuleMapper<ID, IDC, R, RRC> mapper) {
         return then(mapper.apply(ruleContext), queryFunction -> queryFunction::apply);
+    }
+
+    static <ID, R, RRC> BatchRuleBuilder<ID, RRC> batchRuleBuilder(
+            Function<R, ID> correlationIdExtractor,
+            RuleMapper<ID, List<ID>, R, RRC> mapper) {
+        return batchRuleBuilder(ruleContext(correlationIdExtractor), mapper);
+    }
+
+    static <ID, IDC extends Collection<ID>, R, RRC> BatchRuleBuilder<ID, RRC> batchRuleBuilder(
+            Function<R, ID> correlationIdExtractor,
+            Supplier<IDC> idCollectionFactory,
+            RuleMapper<ID, IDC, R, RRC> mapper) {
+        return batchRuleBuilder(ruleContext(correlationIdExtractor, idCollectionFactory), mapper);
+    }
+
+    static <ID, IDC extends Collection<ID>, R, RRC> BatchRuleBuilder<ID, RRC> batchRuleBuilder(
+            Function<R, ID> correlationIdExtractor,
+            Supplier<IDC> idCollectionFactory,
+            MapFactory<ID, RRC> mapFactory,
+            RuleMapper<ID, IDC, R, RRC> mapper) {
+        return batchRuleBuilder(ruleContext(correlationIdExtractor, idCollectionFactory, mapFactory), mapper);
+    }
+
+    static <ID, IDC extends Collection<ID>, R, RRC> BatchRuleBuilder<ID, RRC> batchRuleBuilder(
+            RuleContext<ID, IDC, R, RRC> ruleContext,
+            RuleMapper<ID, IDC, R, RRC> mapper) {
+
+        return new BatchRuleBuilder<>() {
+
+            @Override
+            public <T> BatchRule<T, RRC> withIdExtractor(Function<T, ID> idExtractor) {
+                return wrap(idExtractor, rule(ruleContext, mapper));
+            }
+        };
+    }
+
+    private static <T, ID, RRC> BatchRule<T, RRC> wrap(Function<T, ID> idExtractor, Rule<ID, RRC> rule) {
+
+        return entities -> {
+            final Map<ID, T> entityMap = toStream(entities)
+                    .collect(toMap(idExtractor, identity(), (o, o2) -> o2, LinkedHashMap::new));
+
+            return rule.apply(entityMap.keySet())
+                    .map(resultMap -> resultMap.entrySet()
+                            .stream()
+                            .collect(toMap(m -> entityMap.get(m.getKey()), Entry::getValue, (o, o2) -> o2, LinkedHashMap::new)));
+        };
+    }
+
+    @FunctionalInterface
+    interface BatchRule<T, RRC> {
+
+        Mono<Map<T, RRC>> toMono(Iterable<T> entities);
+
+        default Flux<RRC> toFlux(Iterable<T> entities) {
+            return toMono(entities)
+                    .flatMapMany(resultMap -> fromIterable(resultMap.values()));
+        }
+    }
+
+    interface BatchRuleBuilder<ID, RRC> {
+        <T> BatchRule<T, RRC> withIdExtractor(Function<T, ID> idExtractor);
     }
 }
