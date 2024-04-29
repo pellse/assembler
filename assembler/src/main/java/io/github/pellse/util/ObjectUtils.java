7636fbae432c4572c2f03098c9bd4d32564f096e
pellse <sebastien.pelletier@sekmed.ca>
2024-04-29 16:38:48 -0400

    Removed unused import

--- assembler/src/main/java/io/github/pellse/util/ObjectUtils.java
+++ assembler/src/main/java/io/github/pellse/util/ObjectUtils.java
@@ -17,7 +17,6 @@
 package io.github.pellse.util;
 
 import java.util.Objects;
-import java.util.Optional;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
