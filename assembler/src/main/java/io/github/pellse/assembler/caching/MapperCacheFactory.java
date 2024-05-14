13051f936ab6db26c212c154e26691cb74eda41e
pellse <sebastien.pelletier@sekmed.ca>
2024-05-14 14:35:49 -0400

    #30 `MapperCacheFactory` now apply `mappingFunction` to fetch function in `Cache.computeAll`

--- assembler/src/main/java/io/github/pellse/assembler/caching/MapperCacheFactory.java
+++ assembler/src/main/java/io/github/pellse/assembler/caching/MapperCacheFactory.java
@@ -11,17 +11,17 @@ import static io.github.pellse.util.collection.CollectionUtils.transformMap;
 
 public interface MapperCacheFactory {
 
-    static <ID, EID, R, RRC, CTX extends CacheContext<ID, EID, R, RRC>> CacheTransformer<ID, EID, R, RRC, CTX> mapper(Function<CTX, BiFunction<ID, RRC, RRC>> contextMappingFunction) {
-        return cacheFactory -> mapper(cacheFactory, contextMappingFunction);
+    static <ID, EID, R, RRC, CTX extends CacheContext<ID, EID, R, RRC>> CacheTransformer<ID, EID, R, RRC, CTX> mapper(Function<CTX, BiFunction<ID, RRC, RRC>> mappingFunction) {
+        return cacheFactory -> mapper(cacheFactory, mappingFunction);
     }
 
-    static <ID, EID, R, RRC, CTX extends CacheContext<ID, EID, R, RRC>> CacheFactory<ID, EID, R, RRC, CTX> mapper(CacheFactory<ID, EID, R, RRC, CTX> cacheFactory, Function<CTX, BiFunction<ID, RRC, RRC>> contextMappingFunction) {
+    static <ID, EID, R, RRC, CTX extends CacheContext<ID, EID, R, RRC>> CacheFactory<ID, EID, R, RRC, CTX> mapper(CacheFactory<ID, EID, R, RRC, CTX> cacheFactory, Function<CTX, BiFunction<ID, RRC, RRC>> mappingFunction) {
         return context -> then(cacheFactory.create(context), delegateCache -> adapterCache(
                 delegateCache::getAll,
-                delegateCache::computeAll,
-                map -> delegateCache.putAll(transformMap(map, contextMappingFunction.apply(context))),
+                (ids, fetchFunction) -> delegateCache.computeAll(ids, idList -> fetchFunction.apply(idList).map(m -> transformMap(m, mappingFunction.apply(context)))),
+                map -> delegateCache.putAll(transformMap(map, mappingFunction.apply(context))),
                 delegateCache::removeAll,
-                (mapToAdd, mapToRemove) -> delegateCache.updateAll(transformMap(mapToAdd, contextMappingFunction.apply(context)), mapToRemove)
+                (mapToAdd, mapToRemove) -> delegateCache.updateAll(transformMap(mapToAdd, mappingFunction.apply(context)), mapToRemove)
         ));
     }
 }
