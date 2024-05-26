4692f4d49b23a42150685c26be780732ff36591d
pellse <sebastien.pelletier@sekmed.ca>
2024-05-25 21:04:58 -0400

    #30 Adding onUpdateAll consumer to ObservableCacheFactory

--- assembler/src/main/java/io/github/pellse/assembler/caching/ObservableCacheFactory.java
+++ assembler/src/main/java/io/github/pellse/assembler/caching/ObservableCacheFactory.java
@@ -4,6 +4,7 @@ import io.github.pellse.assembler.caching.CacheFactory.CacheTransformer;
 import reactor.core.publisher.Mono;
 
 import java.util.Map;
+import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 import java.util.function.Function;
 
@@ -15,7 +16,8 @@ public interface ObservableCacheFactory {
             Consumer<Map<ID, RRC>> onGetAll,
             Consumer<Map<ID, RRC>> onComputeAll,
             Consumer<Map<ID, RRC>> onPutAll,
-            Consumer<Map<ID, RRC>> onRemoveAll) {
+            Consumer<Map<ID, RRC>> onRemoveAll,
+            BiConsumer<Map<ID, RRC>, Map<ID, RRC>> onUpdateAll) {
 
         return cacheFactory -> cacheContext -> {
 
@@ -25,7 +27,8 @@ public interface ObservableCacheFactory {
                     ids -> cache.getAll(ids).transform(call(onGetAll)),
                     (ids, fetchFunction) -> cache.computeAll(ids, fetchFunction).transform(call(onComputeAll)),
                     map -> cache.putAll(map).transform(call(map, onPutAll)),
-                    map -> cache.removeAll(map).transform(call(map, onRemoveAll))
+                    map -> cache.removeAll(map).transform(call(map, onRemoveAll)),
+                    (mapToAdd, mapToRemove) -> cache.updateAll(mapToAdd, mapToRemove).transform(call(mapToAdd, mapToRemove, onUpdateAll))
             );
         };
     }
@@ -36,5 +39,9 @@ public interface ObservableCacheFactory {
 
     static <T, U> Function<Mono<T>, Mono<T>> call(U value, Consumer<U> consumer) {
         return mono -> consumer != null ? mono.doOnNext(__ -> consumer.accept(value)) : mono;
+    }
+
+    static <T, U, V> Function<Mono<T>, Mono<T>> call(U value1, V value2, BiConsumer<U, V> consumer) {
+        return mono -> consumer != null ? mono.doOnNext(__ -> consumer.accept(value1, value2)) : mono;
     }
 }
