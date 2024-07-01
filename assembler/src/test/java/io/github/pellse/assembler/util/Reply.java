02e612d2ccee8a42657139c527f0a8a1742d07dd
pellse <sebastien.pelletier@sekmed.ca>
2024-06-30 22:35:47 -0400

    #33 Reply.userId now a String in test package

--- assembler/src/test/java/io/github/pellse/assembler/util/Reply.java
+++ assembler/src/test/java/io/github/pellse/assembler/util/Reply.java
@@ -1,4 +1,4 @@
 package io.github.pellse.assembler.util;
 
-public record Reply(Long id, Long postId, Long userId, String content) {
+public record Reply(Long id, Long postId, String userId, String content) {
 }
