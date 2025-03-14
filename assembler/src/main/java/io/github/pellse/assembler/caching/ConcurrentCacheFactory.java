d758d3fb1f3874d09d06c763f405eda2cf69a4cd
pellse <sebastien.pelletier@sekmed.ca>
2025-03-13 23:30:13 -0400

    Adding factory methods to `ConcurrentCacheFactory` for event notification registration

--- assembler/src/main/java/io/github/pellse/assembler/caching/ConcurrentCacheFactory.java
+++ assembler/src/main/java/io/github/pellse/assembler/caching/ConcurrentCacheFactory.java
@@ -17,12 +17,19 @@
 package io.github.pellse.assembler.caching;
 
 import io.github.pellse.assembler.caching.CacheFactory.CacheTransformer;
+import io.github.pellse.concurrent.Lock;
 import io.github.pellse.concurrent.LockStrategy;
 import io.github.pellse.concurrent.ReactiveGuard.ReactiveGuardBuilder;
+import io.github.pellse.concurrent.ReactiveGuardEvent;
 import reactor.core.scheduler.Scheduler;
+
+import java.util.Optional;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
 
 import static io.github.pellse.assembler.caching.ConcurrentCache.concurrentCache;
 import static io.github.pellse.concurrent.ReactiveGuard.reactiveGuardBuilder;
+import static io.github.pellse.concurrent.ReactiveGuardEventListener.reactiveGuardEventAdapter;
 
 public interface ConcurrentCacheFactory {
 
@@ -40,6 +47,14 @@ public interface ConcurrentCacheFactory {
 
     static <ID, R, RRC, CTX extends CacheContext<ID, R, RRC, CTX>> CacheTransformer<ID, R, RRC, CTX> concurrent(LockStrategy lockStrategy, Scheduler fetchFunctionScheduler) {
         return concurrent(reactiveGuardBuilder().lockingStrategy(lockStrategy), fetchFunctionScheduler);
+    }
+
+    static <ID, R, RRC, CTX extends CacheContext<ID, R, RRC, CTX>> CacheTransformer<ID, R, RRC, CTX> concurrent(Consumer<ReactiveGuardEvent> eventConsumer) {
+        return concurrent(reactiveGuardBuilder().eventListener(reactiveGuardEventAdapter(eventConsumer)));
+    }
+
+    static <ID, R, RRC, CTX extends CacheContext<ID, R, RRC, CTX>> CacheTransformer<ID, R, RRC, CTX> concurrent(BiConsumer<ReactiveGuardEvent, Optional<Lock<?>>> eventConsumer) {
+        return concurrent(reactiveGuardBuilder().eventListener(reactiveGuardEventAdapter(eventConsumer)));
     }
 
     static <ID, R, RRC, CTX extends CacheContext<ID, R, RRC, CTX>> CacheTransformer<ID, R, RRC, CTX> concurrent(ReactiveGuardBuilder reactiveGuardBuilder) {
