4ac26ea0dc6bf3b6fdd89a9963eb7be619575f73
pellse <sebastien.pelletier@sekmed.ca>
2025-03-14 10:56:56 -0400

    Adding long-running concurrency test

--- assembler-spring-cache/src/test/java/io/github/pellse/assembler/caching/spring/SpringCacheAssemblerTest.java
+++ assembler-spring-cache/src/test/java/io/github/pellse/assembler/caching/spring/SpringCacheAssemblerTest.java
@@ -7,6 +7,7 @@ import io.github.pellse.assembler.util.OrderItem;
 import io.github.pellse.assembler.util.Transaction;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
 import org.reactivestreams.Publisher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -20,7 +21,9 @@ import reactor.test.StepVerifier;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Consumer;
 import java.util.function.Function;
 
 import static io.github.pellse.assembler.AssemblerBuilder.assemblerOf;
@@ -32,17 +35,24 @@ import static io.github.pellse.assembler.caching.StreamTableFactory.streamTable;
 import static io.github.pellse.assembler.caching.StreamTableFactoryBuilder.streamTableBuilder;
 import static io.github.pellse.assembler.caching.CacheFactory.cached;
 import static io.github.pellse.assembler.caching.CacheFactory.cachedMany;
+import static io.github.pellse.assembler.caching.spring.SpringCacheAssemblerTest.CDCAdd.cdcAdd;
+import static io.github.pellse.assembler.caching.spring.SpringCacheAssemblerTest.CDCDelete.cdcDelete;
 import static io.github.pellse.assembler.caching.spring.SpringCacheFactory.springCache;
 import static io.github.pellse.assembler.test.AssemblerTestUtils.*;
 import static io.github.pellse.assembler.test.AssemblerTestUtils.transaction1;
 import static io.github.pellse.util.ObjectUtils.also;
 import static io.github.pellse.util.collection.CollectionUtils.transform;
 import static io.github.pellse.util.reactive.ReactiveUtils.defaultScheduler;
+import static io.github.pellse.util.reactive.ReactiveUtils.scheduler;
+import static java.lang.Integer.MAX_VALUE;
+import static java.lang.Thread.currentThread;
 import static java.time.Duration.ofMillis;
 import static java.util.List.of;
+import static java.util.concurrent.locks.LockSupport.parkNanos;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static reactor.core.scheduler.Schedulers.boundedElastic;
-import static reactor.core.scheduler.Schedulers.parallel;
+import static reactor.core.publisher.Flux.fromIterable;
+import static reactor.core.scheduler.Schedulers.*;
+import static reactor.core.scheduler.Schedulers.newBoundedElastic;
 
 @SpringBootTest
 public class SpringCacheAssemblerTest {
@@ -59,6 +69,22 @@ public class SpringCacheAssemblerTest {
             return also(new CaffeineCacheManager(),
                     cacheManager -> cacheManager.setAsyncCacheMode(true),
                     cacheManager -> cacheManager.setCacheNames(of(BILLING_INFO_CACHE, ORDER_ITEMS_CACHE)));
+        }
+    }
+
+    sealed interface CDC<T> {
+        T item();
+    }
+
+    record CDCAdd<T>(T item) implements CDC<T> {
+        static <T> CDC<T> cdcAdd(T item) {
+            return new CDCAdd<>(item);
+        }
+    }
+
+    record CDCDelete<T>(T item) implements CDC<T> {
+        static <T> CDC<T> cdcDelete(T item) {
+            return new CDCDelete<>(item);
         }
     }
 
@@ -101,6 +127,93 @@ public class SpringCacheAssemblerTest {
 
         cacheManager.getCacheNames()
                 .forEach(name -> cacheManager.getCache(name).invalidate());
+    }
+
+    @Test
+    @Timeout(60)
+    public void testLongRunningAutoCachingEvents() throws InterruptedException {
+
+        BillingInfo updatedBillingInfo2 = new BillingInfo(2, 2L, "4540222222222222");
+        OrderItem updatedOrderItem11 = new OrderItem("1", 1L, "Sweater", 25.99);
+        OrderItem updatedOrderItem22 = new OrderItem("5", 2L, "Boots", 109.99);
+
+        final AtomicInteger billingInvocationCount = new AtomicInteger();
+        final AtomicInteger ordersInvocationCount = new AtomicInteger();
+
+        var customerList = List.of(customer1, customer2, customer3);
+
+        var billingInfoList = List.of(cdcAdd(billingInfo1), cdcAdd(billingInfo2), cdcAdd(updatedBillingInfo2), cdcAdd(billingInfo3));
+
+        var orderItemList = List.of(cdcAdd(orderItem11), cdcAdd(orderItem12), cdcAdd(orderItem13),
+                cdcAdd(orderItem21), cdcAdd(orderItem22), cdcAdd(updatedOrderItem22),
+                cdcAdd(orderItem31), cdcAdd(orderItem32), cdcAdd(orderItem33),
+                cdcDelete(orderItem31), cdcDelete(orderItem32), cdcAdd(updatedOrderItem11));
+
+        var customerScheduler = scheduler(() -> newBoundedElastic(4, MAX_VALUE, "Customer-Scheduler"));
+        var billingInfoScheduler = scheduler(() -> newBoundedElastic(4, MAX_VALUE, "BillingInfo-Write-Scheduler"));
+        var orderItemScheduler = scheduler(() -> newBoundedElastic(4, MAX_VALUE, "OrderItem-Write-Scheduler"));
+
+        var customerFlux = fromIterable(customerList).repeat();
+        var billingInfoFlux = fromIterable(billingInfoList).repeat().delayElements(ofMillis(1), billingInfoScheduler);
+        var orderItemFlux = fromIterable(orderItemList).repeat().delayElements(ofMillis(1), orderItemScheduler);
+
+        Function<String, Consumer<Object>> simulateIO = tag -> __  -> {
+            parkNanos(ofMillis(600).toNanos()); // Simulate blocking I/O
+            System.out.println(currentThread().getName() + ": " + tag);
+        };
+
+        Function<List<Customer>, Publisher<BillingInfo>> getBillingInfo = customers -> {
+
+            var customerIds = transform(customers, Customer::customerId);
+
+            return Flux.just(billingInfo1, billingInfo2, billingInfo3)
+                    .filter(billingInfo -> customerIds.contains(billingInfo.customerId()))
+                    .doOnSubscribe(simulateIO.apply("BillingInfo"))
+                    .doOnComplete(billingInvocationCount::incrementAndGet);
+        };
+
+        Function<List<Customer>, Publisher<OrderItem>> getOrderItems = customers -> {
+
+            var customerIds = transform(customers, Customer::customerId);
+
+            return Flux.just(orderItem11, orderItem12, orderItem13, orderItem21, orderItem22)
+                    .filter(orderItem -> customerIds.contains(orderItem.customerId()))
+                    .doOnSubscribe(simulateIO.apply("OrderItem"))
+                    .doOnComplete(ordersInvocationCount::incrementAndGet);
+        };
+
+        var assembler = assemblerOf(Transaction.class)
+                .withCorrelationIdResolver(Customer::customerId)
+                .withRules(
+                        rule(BillingInfo::customerId, oneToOne(cached(getBillingInfo, springCache(cacheManager, BILLING_INFO_CACHE),
+                                streamTableBuilder(billingInfoFlux, CDCAdd.class::isInstance, CDC::item)
+                                        .maxWindowSize(3)
+                                        .concurrent()
+                                        .build()))),
+                        rule(OrderItem::customerId, oneToMany(OrderItem::id, cachedMany(getOrderItems, springCache(cacheManager, ORDER_ITEMS_CACHE),
+                                streamTableBuilder(orderItemFlux, CDCAdd.class::isInstance, CDC::item)
+                                        .maxWindowSize(3)
+                                        .concurrent()
+                                        .build()))),
+                        Transaction::new)
+                .build();
+
+        var transactionFlux = customerFlux
+                .delayElements(ofMillis(1), customerScheduler)
+                .window(3)
+                .flatMapSequential(assembler::assemble)
+                .take(1_000);
+
+        var initialCount = 500;
+        var latch = new CountDownLatch(initialCount);
+
+        for (int i = 0; i < initialCount; i++) {
+            transactionFlux.doFinally(signalType -> latch.countDown())
+                    .subscribe();
+        }
+        latch.await();
+
+        System.out.println("getBillingInfo invocation count: " + billingInvocationCount.get() + ", getOrderItems invocation count: " + ordersInvocationCount.get());
     }
 
     @Test
