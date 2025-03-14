aebfaaecba1124ca2c179b6d4ce1efcd94d8910d
pellse <sebastien.pelletier@sekmed.ca>
2025-03-14 15:07:28 -0400

    Introducing a new AsyncCacheFactory decorator that provides an asynchronous facade for non-async cache implementations, enabling support for async "computeIfAbsent" semantics in caches that don't natively offer it.

---
+++ assembler/src/main/java/io/github/pellse/assembler/caching/AsyncCacheFactory.java
@@ -0,0 +1,48 @@
+package io.github.pellse.assembler.caching;
+
+import io.github.pellse.assembler.caching.Cache.FetchFunction;
+import reactor.core.publisher.Mono;
+
+import java.util.Map;
+import java.util.Set;
+import java.util.function.BiFunction;
+
+import static io.github.pellse.assembler.caching.Cache.adapterCache;
+import static io.github.pellse.util.collection.CollectionUtils.diff;
+import static io.github.pellse.util.collection.CollectionUtils.isEmpty;
+import static java.util.concurrent.ConcurrentHashMap.newKeySet;
+
+public interface AsyncCacheFactory {
+
+    static <ID, R, RRC, CTX extends CacheContext<ID, R, RRC, CTX>> CacheFactory.CacheTransformer<ID, R, RRC, CTX> async() {
+        return AsyncCacheFactory::async;
+    }
+
+    static <ID, R, RRC, CTX extends CacheContext<ID, R, RRC, CTX>> CacheFactory<ID, R, RRC, CTX> async(CacheFactory<ID, R, RRC, CTX> cacheFactory) {
+        return context -> async(cacheFactory.create(context));
+    }
+
+    static <ID, RRC> Cache<ID, RRC> async(Cache<ID, RRC> delegateCache) {
+
+        final Set<ID> idSet = newKeySet();
+
+        BiFunction<Iterable<ID>, FetchFunction<ID, RRC>, Mono<Map<ID, RRC>>> computeAll = (ids, fetchFunction) -> {
+            final var missingIds = diff(ids, idSet);
+            if (isEmpty(missingIds)) {
+                return delegateCache.getAll(ids);
+            }
+
+            idSet.addAll(missingIds);
+
+            return delegateCache.computeAll(ids, fetchFunction)
+                    .doOnNext(map -> idSet.removeAll(diff(idSet, map.keySet())));
+        };
+
+        return adapterCache(
+                delegateCache::getAll,
+                computeAll,
+                delegateCache::putAll,
+                delegateCache::removeAll,
+                delegateCache::updateAll);
+    }
+}
