e712df3e0b86b502cba462456939bb9a49dd2ebe
pellse <sebastien.pelletier@sekmed.ca>
2025-05-15 23:13:45 -0400

    `MergeFunction` interface now has a dedicated `merge` single abstract method

--- assembler/src/main/java/io/github/pellse/assembler/caching/merge/MergeFunction.java
+++ assembler/src/main/java/io/github/pellse/assembler/caching/merge/MergeFunction.java
@@ -4,4 +4,10 @@ import io.github.pellse.util.function.Function3;
 
 @FunctionalInterface
 public interface MergeFunction<ID, RRC> extends Function3<ID, RRC, RRC, RRC> {
+
+    RRC merge(ID id, RRC existingValue, RRC newValue);
+
+    default RRC apply(ID id, RRC existingValue, RRC newValue) {
+        return merge(id, existingValue, newValue);
+    }
 }
